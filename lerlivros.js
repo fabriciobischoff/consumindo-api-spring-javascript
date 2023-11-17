25f3e56df04789e57c134c87875fc1cf4e4fb688
Fabricio <fabriciobischoff27@gmail.com>
2023-11-17 17:52:08 -0300

    modificando função de consulta de livros

--- lerlivros.js
+++ lerlivros.js
@@ -1,4 +1,6 @@
-fetch('http://localhost:8080/linguagens')
-    .then(response => response.json())
-    .then(r => console.log(r))
-    .catch((error) => {console.log(error)})
+const json = fetch('http://localhost:8080/linguagens')
+    .then(resposta => resposta.json())
+
+    json.then(dados => {
+        console.log(dados)
+    })
