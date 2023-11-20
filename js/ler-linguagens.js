adbf97ec6e961c35bc289a774006b654da535c0a
Fabricio <fabriciobischoff27@gmail.com>
2023-11-20 17:22:33 -0300

    adicionando function para ler-linguagens

---
+++ js/ler-linguagens.js
@@ -0,0 +1,23 @@
+function carregar() {
+    fetch('http://localhost:8080/linguagens')
+    .then(response => response.json())
+    .then(linguagens => {
+        const container = document.querySelector("#linguagens-container")
+
+        linguagens.map(linguagem => {
+            const card = document.createElement("div")
+            card.classList.add("card")
+
+            const img = document.createElement("img");
+            img.src = linguagem.pathFoto
+            img.alt = linguagem.titulo
+
+            const titulo = document.createElement("h3")
+            titulo.textContent = linguagem.titulo
+
+            card.appendChild(img)
+            card.appendChild(titulo)
+            container.appendChild(card)
+        })
+    })
+}
--- js/lerlivros.js
+++
@@ -1,6 +0,0 @@
-const json = fetch('http://localhost:8080/linguagens')
-    .then(resposta => resposta.json())
-
-    json.then(dados => {
-        console.log(dados)
-    })
